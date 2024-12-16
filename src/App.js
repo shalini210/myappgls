1ca03fdd3516ac0f4769042ab2df1b53e4ba46de
shalini <s.rathore43@gmail.com>
2024-12-16 11:14:13 +0530

    rew

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Task from './components/Task';
+
+function App() {
+  return (
+    <div className="App">
+      <Task></Task>
+    </div>
+  );
+}
+
+export default App;
